ed95d2718b9bbc1013e810a5edc340f2eab92559
Rashid Siregar <74960869+rashidsiregar28@users.noreply.github.com>
2021-08-26 18:14:04 +0700

    Hdeh

--- main.js
+++ main.js
@@ -26,13 +26,9 @@ xinz.multi = true
 xinz.nopref = false
 xinz.prefa = 'anjing'
 
-require('./message/xinz.js')
-nocache('./message/xinz.js', module => console.log(color(`'${module}' Telah berubah!`)))
-
 const start = async(sesion) => {
     xinz.logger.level = 'warn'
 
-    // MENG WE EM
     console.log(color(figlet.textSync('Chika-Bot', {
 		font: 'Standard',
 		horizontalLayout: 'default',
@@ -41,13 +37,11 @@ const start = async(sesion) => {
 	}), 'cyan'))
 	console.log(color('[ CREATED BY XINZTEAM ]'))
 
-    // Menunggu QR
     xinz.on('qr', qr => {
         qrcode.generate(qr, { small: true })
         console.log(XinzLog('Scan QR ~~'))
     })
 
-    // Restore Sesion
     fs.existsSync(sesion) && xinz.loadAuthInfo(sesion)
 
     // Mencoba menghubungkan
@@ -55,21 +49,17 @@ const start = async(sesion) => {
 		console.log(XinzLog('Connecting...'))
 	})
 
-    // Konek
     xinz.on('open', (json) => {
 		console.log(XinzLog('Connect, Welcome Owner'))
 	})
 
-    // Write Sesion
     await xinz.connect({timeoutMs: 30*1000})
     fs.writeFileSync(sesion, JSON.stringify(xinz.base64EncodedAuthInfo(), null, '\t'))
 
-    // Ya gitulah
     xinz.on('ws-close', () => {
         console.log(XinzLog('Koneksi terputus, mencoba menghubungkan kembali..'))
     })
 
-    // Ntahlah
     xinz.on('close', async ({ reason, isReconnecting }) => {
         console.log(XinzLog('Terputus, Alasan :' + reason + '\nMencoba mengkoneksi ulang :' + isReconnecting))
         if (!isReconnecting) {
@@ -77,35 +67,26 @@ const start = async(sesion) => {
         }
     })
 
-    // Block
     xinz.on('CB:Blocklist', json => {
         if (blocked.length > 2) return
         for (let i of json[1].blocklist) {
             blocked.push(i.replace('c.us','s.whatsapp.net'))
         }
     })
-
-    // Action Call
     xinz.on('CB:action,,call', async json => {
         const callerid = json[2][0][1].from;
         xinz.sendMessage(callerid, `Maaf bot tidak menerima call`, MessageType.text)
         await xinz.blockUser(callerid, "add")
     })
-
-    // Action Battery
     xinz.on('CB:action,,battery', json => {
         const a = json[2][0][1].value
         const b = json[2][0][1].live
         xinz.baterai.baterai = a
         xinz.baterai.cas = b
     })
-
-    // Anti delete
     xinz.on('message-delete', async(json) => {
         require('./message/antidelete')(xinz, json)
     })
-
-    // Chat
     xinz.on('chat-update', async (qul) => {
 		if (!qul.hasNewMessage) return
         qul = qul.messages.all()[0]
@@ -114,30 +95,11 @@ const start = async(sesion) => {
         let msg = serialize(xinz, qul)
         let smsg = serializeM(xinz, qul)
 		require('./message/xinz')(xinz, msg, smsg, blocked, _afk, welcome)
-	})
-
-    // Event Group 
+	}) 
     xinz.on('group-participants-update', async (anj) => {
         require("./message/group")(xinz, anj, welcome)
     })
 }
-/**
- * Uncache if there is file change
- * @param {string} module Module name or path
- * @param {function} cb <optional> 
- */
- function nocache(module, cb = () => { }) {
-    console.log(color(`Module ${module} Dipantau oleh kang Bakso`))
-    fs.watchFile(require.resolve(module), async () => {
-        await uncache(require.resolve(module))
-        cb(module)
-    })
-}
-
-/**
- * Uncache a module
- * @param {string} module Module name or path
- */
 function uncache(module = '.') {
     return new Promise((resolve, reject) => {
         try {
