d5c49c7eec4a1a57bcc3bbadb2a853f8d0fbb5b4
Rashid Siregar <74960869+rashidsiregar28@users.noreply.github.com>
2021-11-21 05:25:03 +0700

    Update ind.js

--- help/ind.js
+++ help/ind.js
@@ -1,49 +1,29 @@
-const fs = require('fs')
+const fs = require('fs-extra')
 
+exports.wait = () => {
+    return `⏳ Mohon tunggu sebentar~`
+}
 
 exports.ok = () => {
-    return `Ok desu~`
+    return `✅ Done. Ok desu~`
+}
+
+exports.err = () => {
+    return `‼️Fitur Sedang Error‼️
+
+⏳Sedang melapor bug fitur ke owner-sama⏳`
+}
+
+exports.wrongFormat = (prefix) => {
+    return `Format salah ‼️ Silakan cek cara penggunaan di *${prefix}allmenu*.`
+}
+
+exports.emptyMess = () => {
+    return `Harap masukkan pesan yang ingin disampaikan!`
 }
 
 exports.cmdNotFound = (cmd, prefix) => {
-    return `Command *${cmd}* tidak terdaftar di *${prefix}menu*`
-}
-
-exports.hadis = (command) => {
-    return `*── 「 HADIS 」 ──*
-
-Daftar hadis:
-1. Hadis Bukhari ada 6638 hadis
-    _usage_: ${command} bukhari 1
-2. Hadis Muslim ada 4930 hadis
-    _usage_: ${command} muslim 25
-3. Hadis Tirmidzi ada 3625 hadis
-    _usage_: ${command} tirmidzi 10
-4. Hadis nasai ada 5364 hadis
-    _usage_: ${command} nasai 6
-5. Hadis Ahmad ada 4305 hadis
-    _usage_: ${command} ahmad 5
-6. Hadis Abu Daud ada 4419 hadis
-    _usage_: ${command} abu-daud 45
-7. Hadis Malik ada 1587 hadis
-    _usage_: ${command} malik 45
-8. Hadis Ibnu Majah ada 4285 hadis
-    _usage_: ${command} ibnu-majah 8
-9. Hadis Darimi ada 2949 hadis
-    _usage_: ${command} darimi 3`
-}
-
-exports.groupBot = (sender) => {
-    return `
-Link Group Chika Fenst : https://chat.whatsapp.com/HLU6B1Mw34QBMUoXAyhec0
-Link Group Chika Fenst V2 : https://chat.whatsapp.com/IwGkynIvQAk1J6wQVDLeFC
-
-
-Jangan lupa join ya kak @${sender.split('@')[0]}`
-}
-
-exports.BlockBan = (ownerNumber, prefix) => {
-    return `*Sepertinya kamu telah diblok/diban. Hubungi untuk mengetahui cara membuka blok!*\n\n Owner: wa.me/${ownerNumber}`
+    return `Command *${cmd}* tidak terdaftar di *${prefix}allmenu*`
 }
 
 exports.ownerOnly = () => {
@@ -58,79 +38,18 @@ exports.groupOnly = () => {
     return `Command ini hanya bisa digunakan di dalam grup!`
 }
 
-exports.notNsfw = () => {
-    return `Command NSFW belum diaktifkan!`
+exports.adminOnly = () => {
+    return `Command ini hanya bisa digunakan oleh admin grup!`
 }
 
-exports.nsfwOn = () => {
-    return `Command NSFW berhasil *diaktifkan*!`
+exports.nhFalse = () => {
+    return `Kode tidak valid!`
 }
 
-exports.nsfwOff = () => {
-    return `Command NSFW berhasil *dinonaktifkan*!`
-}
-
-exports.nsfwAlready = () => {
-    return `Command NSFW sudah diaktifkan sebelumnya.`
-}
-
-exports.stickerDel = () => {
-    return `Stiker berhasil dihapus dari database!`
-}
-
-exports.stickerAdd = () => {
-    return `Stiker berhasil ditambahkan ke database!`
-}
-
-exports.stickerAddAlready = (q) => {
-    return `Stiker dengan keyword "${q}" sudah ada di database!`
-}
-
-exports.stickerNotFound = () => {
-    return `Stiker tidak ditemukan!`
-}
-
-exports.vnDel = () => {
-    return `Vn berhasil dihapus dari database!`
-}
-
-exports.vnAdd = () => {
-    return `Vn berhasil ditambahkan ke database!`
-}
-
-exports.vnAddAlready = (q) => {
-    return `Vn dengan keyword "${q}" sudah ada di database!`
-}
-
-exports.vnNotFound = () => {
-    return `Vn tidak ditemukan!`
-}
-
-exports.imageAdd = () => {
-    return `image berhasil ditambahkan ke database!`
-}
-
-exports.imageAddAlready = (q) => {
-    return `image dengan keyword "${q}" sudah ada di database!`
-}
-
-exports.imageNotFound = () => {
-    return `image tidak ditemukan!`
-}
-
-exports.respon = (ketikan, respt) => {
-    return `
-*── 「 AUTO RESPON 」 ──*
-
-Auto-Respon berhasil dibuat dengan:
-
-➸ *Text*: ${ketikan}
-➸ *Akan Merespon*: ${respt}`
-}
-
-exports.responAlready = (ketikan) => {
-    return `
-Maaf, autorespon untuk teks "${ketikan}" sudah ada sebelumnya,harap memasukkan teks lain.`
+exports.listBlock = (blockNumber) => {
+    return `*── 「 HALL OF SHAME 」 ──*
+    
+Total diblokir: *${blockNumber.length}* user\n`
 }
 
 exports.received = (pushname) => {
@@ -139,241 +58,8 @@ Halo ${pushname}!
 Terima kasih telah melapor, laporanmu akan kami segera terima.`
 }
 
-exports.levelingOn = () => {
-    return `Fitur leveling berhasil *diaktifkan*!`
-}
-
-exports.levelingOff = () => {
-    return `Fitur leveling berhasil *dinonaktifkan*!`
-}
-
-exports.levelingOnAlready = () => {
-    return `Fitur leveling telah diaktifkan sebelumnya.`
-}
-
-exports.levelingNotOn = () => {
-    return `Fitur leveling belum diaktifkan!`
-}
-
-exports.claimOn = (yo) => {
-    return `
-*── 「 CLAIM  」 ──*
-
-Selamat kamu mendapatkan *10000Xp* dan *$${yo}* balance
-Dari claim harian`
-}
-
-exports.claimOnAlready = () => {
-    return `Kamu sudah melakukan claim sebelumnya, Harap claim lagi di esok hari.`
-}
-
 exports.notNum = (q) => {
     return `"${q}", bukan angka!`
-}
-
-exports.menu = (prefix, ucap, pushname2, jumlahUser, timebot, owner, jumlahHarian, jumlahCommand, sender, time, public, levelMenu, xpMenu, reqXp, uangku, role, Limitnya) => {
-    return `\`\`\`Selamat ${ucap} Kak @${sender.split('@')[0]} 😳\`\`\`
-
-╭── 「 *BOT INFO* 」 ──
-│
-│❒͡  _*CHIKA BOT*_ 
-│❒͡ *Contact My Owner*
-│ *https://wa.me/${owner}*
-│❒͡ *Time: ${time}*
-│❒͡ *STATUS* :「 ${public} 」
-│
-╰── 「 *CHIKA BOT* 」 ──
-
-❉──────────────────❉  
-*Name* : *${pushname2}* 
-*Your API* : *https://wa.me/${sender.split('@')[0]}*
-*Limit* : *${Limitnya}*
-
-
-*Your Progress*:
-*Level: ${levelMenu}*
-*XP: ${xpMenu} / ${reqXp}*
-*Role: ${role}*
-*Your Balance: $${uangku}*
-❉──────────────────❉  
-
-*── 「 BOT STATUS 」 ──*
-
- ❏ *${timebot}*
-
- 𒍮 *Auto-Regis User: ${jumlahUser}*
-
- 𒍮 *Total Hit Bot : ${jumlahCommand}*
-
- 𒍮 *Total Hit Today : ${jumlahHarian}*
-
-*── 「 CHIKA BOT 」 ──*
-
-
-*Official BOT By @0*`
-}
-
-exports.menuDownloader = (prefix) => {
-    return `
-*── 「 DOWNLOADER 」 ──*
-
-    `
-}
-
-exports.menuText = (prefix) => {
-    return `
-*── 「 TEXT MAKER 」 ──*
-    `
-}
-
-exports.menuPremi = (prefix) => {
-    return `
-*── 「 PREMIUM 」 ──*
-Halo premium users ヽ(・∀・)ﾉ!
-`
-}
-
-exports.menuSticker = (prefix) => {
-    return `
-*── 「 STICKER 」 ──*
-    `
-}
-
-exports.menuWeeaboo = (prefix) => {
-    return `
-*── 「 WEEABOO 」 ──*
-
-    `
-}
-
-exports.menuFun = (prefix) => {
-    return `
-*── 「 FUN FEATURE 」 ──*
-
-    `
-}
-
-exports.menuImage = (prefix) => {
-    return `
-*── 「 EFFECT AND MANIPULATION 」 ──*
-
-    `
-}
-
-exports.menuKerang = (prefix) => {
-    return `
-*── 「 KERANG 」 ──*
-
-1. *${prefix}kapankah*
-Mengambil jawaban random dari bot.
-Aliases: -
-Usage: *${prefix}kapankah* text
-
-2. *${prefix}bisakah*
-Mengambil jawaban random dari bot.
-Aliases: -
-Usage: *${prefix}bisakah* text
-
-3. *${prefix}rate*
-Mengambil jawaban random dari bot
-Aliases: nilai
-Usage: *${prefix}rate* pertanyaan
-
-
-    `
-}
-
-exports.menuModeration = (prefix) => {
-    return `
-*── 「 MODERATION GROUP 」 ──*
-   `
-}
-
-
-exports.menuNsfw = (prefix) => {
-    return `
-*── 「 NSFW 」 ──*
-
-    `
-}
-
-exports.menuOwner = (prefix) => {
-    return `
-*── 「 OWNER MENU 」 ──*
-Halo Owner-sama ヽ(・∀・)ﾉ!
-   `
-}
-
-exports.menuLeveling = (prefix) => {
-    return `
-*── 「 LEVELING 」 ──*
-    `
-}
-
-exports.menuPrimbon = (prefix) => {
-    return `
-*── 「 PRIMBON MENU 」 ──*
-    `
-}
-
-exports.menuPray = (prefix) => {
-    return `
-*── 「 PRAY 」 ──*
-    `
-}
-
-exports.menuMisc = (prefix) => {
-    return `
-*── 「 MEDIA AND MISC 」 ──*
-
-    `
-}
-
-exports.menuBot = (prefix) => {
-    return `
-*── 「 BOT STAT 」 ──*
-    `
-}
-
-exports.menuPrice = (owner, pushname) => {
-    return `
-*── 「 PRICE LIST 」 ──*
- 
-*Hai kak  ${pushname}*
-*Harga Sewa Bot*
-*15K/Group Selama 1 Bulan*
-
-*Pembayaran Bisa Melalui Metode Pembayaran di Bawah:*
-*OVO/Dana/Gopay = ${owner}*
-*QRis yang dilampirkan*
-*PAYPAL = rashidsiregar28@gmail.com*
-
-*_note_*:
-*Jika kalian ingin menjadi owner bot,bisa chat owner untuk price nya :D*
-*wa.me/${owner} (Owner)*
-
-    `
-}
-
-exports.UpToPremi = (ownerNumber, prefix) => {
-    return `
-*── 「 UPGRADE PREMIUM 」 ──*
-
-*Tarif Premium User adalah 10K Perbulan*
-*Keuntungan Premium Diantaranya:*
-♲ *Bebas memakai fitur premium*
-♲ *Dapat Informasi Lebih dulu akan Update, Nomor Bot Baru (Jika Terbanned), dan Lainnya*
-
-*Jika Tertarik,Kalian Bisa Bayar Melalui Metode Pembayaran di Bawah:*
-*OVO/Dana/Gopay = ${ownerNumber}*
-*Qris pada gambar diatas*
-*PAYPAL = rashidsiregar28@gmail.com*
-
-*Info Lebih Lengkap Chat Owner, Ketik ${prefix}owner
-*_note_*:
-*Pembelian Premium yang disertai SewaBot hanya akan membayar 20K (Diskon 5K)*
-
-    `
 }
 
 exports.rules = (prefix) => {
@@ -401,12 +87,12 @@ exports.tos = (ownerNumber, prefix) => {
 Kalian bisa mendukung saya agar bot ini tetap up to date dengan:
 08127668234 (OVO/Telkomsel/GoPay)
 
+Atau kalian juga bisa donasi melalui QRis diatas.
 
 Terima kasih!
 
 Contact person Owner:
 wa.me/${ownerNumber} (Owner)
 
-Untuk melihat script yang bot gunakan,ketik ${prefix}sc
     `
 }
